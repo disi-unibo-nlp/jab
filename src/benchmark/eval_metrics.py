43bb4f257d596c63e9ca9411d0b7502e36f2c353
Alessio Cocchieri <alessio.cocchieri@outlook.it>
2025-04-07 10:59:39 +0200

    add support fot soft eval

--- src/benchmark/eval_metrics.py
+++ src/benchmark/eval_metrics.py
@@ -10,7 +10,7 @@ from collections import defaultdict
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Script Arguments")
     
-    parser.add_argument("--junit_test_path", type=str, default="out/completions/Qwen2.5-Coder-7B-Instruct/cot/pass10/2025-04-03_11-10-59/junit_results.jsonl", help="Model's HF directory or local path")
+    parser.add_argument("--junit_test_path", type=str, default="out/completions/Qwen2.5-Coder-7B-Instruct/cot/pass10/2025-04-03_11-10-59/junit_results_2.jsonl", help="Model's HF directory or local path")
     parser.add_argument("--out_dir", type=str, default="./out", help="Outputs directory")
     parser.add_argument("--k", type=int, default=10, help="value of K in Pass@k")
 
@@ -67,22 +67,31 @@ if __name__ == "__main__":
         grouped_data[item["id"]].append(item)
 
     # Convert to list of dictionaries
-    grouped_list = [{"id": k, "attempts": v} for k, v in grouped_data.items()]
+    grouped_list = [{"id": id_exam, "attempts": v} for id_exam, v in grouped_data.items()]
 
     logger.info(f"Number of exam session: {len(grouped_list)}")
 
     num_samples = [k] * len(grouped_list)
     num_correct_compilations = []
     num_correct_runtime = []
+    num_correct_runtime_mandatory = []
 
     for exam_session in grouped_list:
         session_attempts = exam_session['attempts']
         compilation_passed = [attempt for attempt in session_attempts if attempt['compilation_passed']]
         runtime_passed = [attempt for attempt in session_attempts if attempt['runtime_passed']]
+        runtime_passed_mandatory = [attempt for attempt in session_attempts if attempt['runtime_passed_mandatory']]
         num_correct_compilations.append(len(compilation_passed))
         num_correct_runtime.append(len(runtime_passed))
+        num_correct_runtime_mandatory.append(len(runtime_passed_mandatory))
     
     compilation_k = format(round(np.mean(estimate_pass_at_k(num_samples, num_correct_compilations, k)) * 100, 1), ".1f")
     pass_k = format(round(np.mean(estimate_pass_at_k(num_samples, num_correct_runtime, k)) * 100, 1), ".1f")
-    print(f"Mean compilation@{k}:", compilation_k)
-    print(f"Mean pass@{k}:", pass_k)
+    pass_k_mandatory = format(round(np.mean(estimate_pass_at_k(num_samples, num_correct_runtime_mandatory, k)) * 100, 1), ".1f")
+
+    logger.info("-"*25)
+    logger.info(f"Mean compilation@{k}: {compilation_k}")
+    logger.info(f"Mean pass@{k} - SOFT: {pass_k_mandatory}")
+    logger.info(f"Mean pass@{k} - HARD: {pass_k}")
+    logger.info("-"*25)
+    
